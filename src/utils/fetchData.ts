7523d0668318838a7da905d0110e2e609e8b7ebe
Siddheshs07 <siddeshsalimath07@gmail.com>
2024-06-12 11:16:56 +0530

    made changes in fetch data file file

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -1,40 +1,40 @@
-import { dataProps } from "@/types";
-import axios, { AxiosResponse } from "axios";
+import { dataProps } from '@/types';
+import axios, { AxiosResponse } from 'axios';
 
 export async function getAllCloths() {
-  try {
-    const res: AxiosResponse = await axios.get(
-      "http://localhost:3000/api/products"
-    );
-    const product = await res.data;
-    return product;
-  } catch (error) {
-    console.log("Error", error);
-  }
+	try {
+		const res: AxiosResponse = await axios.get(
+			'https://redwolf-3-git-main-siddhesh-salimaths-projects.vercel.app/'
+		);
+		const product = await res.data;
+		return product;
+	} catch (error) {
+		console.log('Error', error);
+	}
 }
 
 export async function getMaleCloths() {
-  const product = await getAllCloths();
-  return product.filter((event: dataProps) => event.gender === "M");
+	const product = await getAllCloths();
+	return product.filter((event: dataProps) => event.gender === 'M');
 }
 export async function getFemaleCloths() {
-  const product = await getAllCloths();
-  return product.filter((event: dataProps) => event.gender === "F");
+	const product = await getAllCloths();
+	return product.filter((event: dataProps) => event.gender === 'F');
 }
 export async function filteredBySize() {
-  const product = await getAllCloths();
-  return product.filter((event: dataProps) => event.productSize);
+	const product = await getAllCloths();
+	return product.filter((event: dataProps) => event.productSize);
 }
 export async function getBrands() {
-  const product = await getAllCloths();
-  return product.filter((event: dataProps) => event.brandName);
+	const product = await getAllCloths();
+	return product.filter((event: dataProps) => event.brandName);
 }
 export async function getFeaturedProducts() {
-  const product = await getAllCloths();
-  return product.filter((event: dataProps) => event.isFeatured);
+	const product = await getAllCloths();
+	return product.filter((event: dataProps) => event.isFeatured);
 }
 export async function getProductById(productId: string) {
-  const product = await getAllCloths();
-  // console.log(product);
-  return product.find((event: dataProps) => event._id === productId);
+	const product = await getAllCloths();
+	// console.log(product);
+	return product.find((event: dataProps) => event._id === productId);
 }
