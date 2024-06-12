c04887f88ffdb31a5322f7e20b0b833b340195f5
Siddheshs07 <siddeshsalimath07@gmail.com>
2024-06-12 10:51:00 +0530

    added tye to multi slider

--- src/components/MultiSlider.tsx
+++ src/components/MultiSlider.tsx
@@ -32,7 +32,7 @@ const MultiSlider = ({
 				arrows={false}
 				className="mb-2"
 			>
-				{slides.map((e, i) => (
+				{slides.map((e: any, i: any) => (
 					<div key={i} className="flex flex-col justify-center items-center">
 						<Image
 							src={e?.url}
