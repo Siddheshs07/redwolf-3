1dda31642a1a2b6ef300c5fe2a1d33e6e64b3059
Siddheshs07 <siddeshsalimath07@gmail.com>
2024-06-12 11:06:43 +0530

    made changes in layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,36 @@
-import type { Metadata } from "next";
-import { Inter, Epilogue } from "next/font/google";
-import "./globals.css";
-import { Footer, Navbar } from "@/components";
-import AppProvider from "../redux/Provider";
-import GoOnTop from "@/components/GoOnTop";
-import ResponsiveNavbar from "@/components/ResponsiveNavbar";
+import { Footer } from '@/components';
+import ResponsiveNavbar from '@/components/ResponsiveNavbar';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import AppProvider from '../redux/Provider';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
-const epilogue = Epilogue({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Cool T-Shirts Online: Designer T-Shirts & Merch in India | Redwolf",
-  description:
-    "Look Alive! Redwolf is an Indie T-Shirt brand selling a line of Cool T-Shirts & Merch Online in India fashioned by Music, Art and All Things Pop Culture.",
+	title: 'Cool T-Shirts Online: Designer T-Shirts & Merch in India | Redwolf',
+	description:
+		'Look Alive! Redwolf is an Indie T-Shirt brand selling a line of Cool T-Shirts & Merch Online in India fashioned by Music, Art and All Things Pop Culture.',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${epilogue.className} ${
-          process.env.NODE_ENV == "development" ? "debug-screens" : ""
-        }`}
-      >
-        <AppProvider>
-          {/* <Navbar /> */}
-          <ResponsiveNavbar />
-          {children}
-          <Footer />
-        </AppProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body
+				className={`${inter.className} ${
+					process.env.NODE_ENV == 'development' ? 'debug-screens' : ''
+				}`}
+			>
+				<AppProvider>
+					<ResponsiveNavbar />
+					{children}
+					<Footer />
+				</AppProvider>
+			</body>
+		</html>
+	);
 }
